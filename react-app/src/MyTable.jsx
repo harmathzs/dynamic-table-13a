9cf4d200be0145923fba8acfa370993258f5a213
Zsolt Harmath <harmathzs@gmail.com>
2025-09-18 11:28:20 +0200

    Finalize task

--- react-app/src/MyTable.jsx
+++ react-app/src/MyTable.jsx
@@ -22,17 +22,27 @@ export default class MyTable extends React.Component {
         rows: []
     }
 
+    handleDelClick = function(rowIdx) {
+        const oldRows = this.state.rows.slice() // shallow copy
+        const newRows = [...oldRows]
+        newRows.splice(rowIdx, 1) // delete
+        this.setState({rows: [...newRows]}) // rerender now
+    }
+
     render() {
         return <div>
             <table style={this.state.style.table}>
                 <tbody>
                     {/*  
                     <tr>
-                        <td style={this.state.style.td}>TODO - {JSON.stringify(this.state.rows)}</td>
+                        <td style={this.state.style.td}>{JSON.stringify(this.state.rows)}</td>
                     </tr>
                     */}
 
                     {this.state.rows.map((word, wordIdx)=><tr key={wordIdx}>
+                        <td>
+                            <button onClick={()=>this.handleDelClick(wordIdx)}>del row</button>
+                        </td>
                         {[...word].map((char, charIdx)=><td key={charIdx} style={this.state.style.td}>
                             {char=='_' ? ' ' : char}
                         </td>)}
